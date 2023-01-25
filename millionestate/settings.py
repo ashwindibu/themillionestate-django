bd33c50a246f56a1b00bc2f7fd26fe664359cd1b
ashwindibu <ashwindibu@gmail.com>
2023-01-25 23:37:19 +0530

    commit 16

--- millionestate/settings.py
+++ millionestate/settings.py
@@ -106,14 +106,21 @@ AUTH_USER_MODEL = 'base.Account'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'real_estate',
+#         'USER': 'root',
+#         'PASSWORD': 'motion@10',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'real_estate',
-        'USER': 'root',
-        'PASSWORD': 'motion@10',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
